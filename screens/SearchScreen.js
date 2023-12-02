d79f0a1cdf6eaff45dab3eceeb322d599fcec9fa
Samuel Abu <abu.sammie@gmail.com>
2023-12-02 13:10:21 +0200

    SearchScreen progress

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -27,37 +27,50 @@ export default function SearchScreen() {
             </View>
 
             {/* search results */}
-            <ScrollView
-                showsVerticalScrollIndicator={false} 
-                contentContainerStyle={{paddingHorizontal:15}}
-                className="space-y-3"
-            >
-                <Text className="text-white font-semibold ml-1">Results ({results.length})</Text>
-                <View className="flex-row justify-between flex-wrap">
-                    {
-                        results.map((item, index) => {
-                            return(
-                                <TouchableWithoutFeedback
-                                    key={index}
-                                    onPress={()=> navigation.push('Movie', item)}
-                                >
-                                    <View className="space-y-2 mb-4">
-                                        <Image 
-                                            source={require('../assets/images/moviePoster2.png')}
-                                            className="rounded-3xl" 
-                                            style={{ width: width*0.44, height: height*0.3}} 
-                                        />
-                                        <Text className="text-gray-300 ml-1">
-                                            {movieName.length > 22 ? movieName.slice(0,22) + '...' : movieName}
-                                        </Text>
-                                    </View>
-                                    
-                                </TouchableWithoutFeedback>
-                            )
-                        })
-                    }
-                </View>
-            </ScrollView>
+            {
+                results.length > 0 ? (
+                    <ScrollView
+                        showsVerticalScrollIndicator={false} 
+                        contentContainerStyle={{paddingHorizontal:15}}
+                        className="space-y-3"
+                    >
+                        <Text className="text-white font-semibold ml-1">Results ({results.length})</Text>
+                        <View className="flex-row justify-between flex-wrap">
+                            {
+                                results.map((item, index) => {
+                                    return(
+                                        <TouchableWithoutFeedback
+                                            key={index}
+                                            onPress={()=> navigation.push('Movie', item)}
+                                        >
+                                            <View className="space-y-2 mb-4">
+                                                <Image 
+                                                    source={require('../assets/images/moviePoster2.png')}
+                                                    className="rounded-3xl" 
+                                                    style={{ width: width*0.44, height: height*0.3}} 
+                                                />
+                                                <Text className="text-gray-300 ml-1">
+                                                    {movieName.length > 22 ? movieName.slice(0,22) + '...' : movieName}
+                                                </Text>
+                                            </View>
+                                            
+                                        </TouchableWithoutFeedback>
+                                    )
+                                })
+                            }
+                        </View>
+                    </ScrollView>
+                ):(
+                    <View className="flex-row justify-center">
+                        <Image 
+                            source={require('../assets/images/movieTime.png')} 
+                            className="h-96 w-96"
+                        />
+                    </View>
+                )
+            }
+
+            
         </SafeAreaView>
     )
 }
